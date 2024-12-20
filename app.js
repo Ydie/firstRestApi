cb70d33f67d9d745e0330d1c402ccee60d6c711f
Ydie <ydie722@gmail.com>
2024-12-20 15:23:16 +0100

    added a CORS lets say auth to make cros servers comunication

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express()
 // app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
 app.use(bodyParser.json()) // application/json
 
+//! CORS Issue fixed by serwer site code. Allow cross access beetwen site and serwer
+//! * means every site is allowed to access
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+	next()
+})
+
 app.use('/feed', feedRoutes)
 
 app.listen(8080)
